1932b9a8a0fa3571ff916ef25e9bcb0d6f089588
Cattimus <82973975+Cattimus@users.noreply.github.com>
2021-05-04 15:43:48 -0700

    added proper MSVC compilation support
    Additionally some functions that are not necessary to be global have been made static

--- src/x_to_json.h
+++ src/x_to_json.h
@@ -9,6 +9,18 @@
 #include <iostream>
 using namespace std;
 
+//MSVC DLL export strangeness
+#ifdef _MSC_VER
+    #ifdef LIBRARY_EXPORT
+        #define EXPORT __declspec(dllexport)
+    #else
+        #define EXPORT __declspec(dllimport)
+    #endif
+#else
+    #define EXPORT
+#endif
+
+
 //TODO - This file in particular has security concerns. Could use a partial or full rewrite in the future
 
 //forward declarations (to prevent circular issues)
@@ -18,18 +30,14 @@ class value;
 
 //change json string to human-readable format
 //indentation seems to fail with very large strings, requires investigation
-string beautify_json(string to_beautify);
-void indent(int level, string* to_indent);
-void remove_indent(string* to_indent);
+EXPORT string beautify_json(string to_beautify);
 
 //create objects
-json_object* create_object(string to_parse);
-json_array*  create_array(string to_parse);
+EXPORT json_object* create_object(string to_parse);
+EXPORT json_array* create_array(string to_parse);
 
 //create a value from string
 value* create_value(string to_parse);
-bool is_num(string input);
-bool is_decimal(string input);
 
 //main parser function
 //rework to fail if the json is not valid
